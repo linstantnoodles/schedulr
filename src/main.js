275e8b5d0a46c7b653f81a4e4913d705c60150e1
alan <linpd2@gmail.com>
2013-01-18 19:07:06 -0500

    moved declarations to top

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 $(window).load(function() {
+
     var OPEN = 1;
     var CLOSED = 0;
+    var schedule_length = 100;
+    var schedule_page = [];
 
-    function Task(a, b, c){ 
+    //task block
+    function Task(a, b, c){
         this.stat = a;
         this.time = b;
         this.priority = c;
@@ -11,13 +15,15 @@ $(window).load(function() {
         this.getPriority = function(){ return this.priority; };
     };
 
-    var schedule_length = 100;
-    var schedule_page = [];
+    //time block
+    function Interval(a, b){ 
+        this.stat = a;
+        this.size = b;
+        this.getStat = function(){ return this.stat; };
+        this.getSize = function(){ return this.size; };
+    };
 
-    //set the closed limits
-    schedule_page[50] = new Task(CLOSED, 5, 0);
-    schedule_page[75] = new Task(CLOSED, 10, 0);
-    //now lets fill in the times
+   //now lets fill in the times
     function getStart(schedule, len){
         for(var i = 0; i < len; i++){
             if (schedule[i] != null)
@@ -26,6 +32,7 @@ $(window).load(function() {
         return 0;
     }
 
+    //prints all schedule info
     function printInfo(schedule){
         for(var i = 0; i < schedule.length; i ++){
             if(schedule[i] != null)
@@ -48,20 +55,27 @@ $(window).load(function() {
         return size;
     }
 
-    var start_limit = getStart(schedule_page, schedule_length); //start at unit 50
-    var curr = start_limit + schedule_page[start_limit].getTime();
+    //set the closed limits
+    schedule_page[50] = new Interval(CLOSED, 5);
+    schedule_page[75] = new Interval(CLOSED, 10);
+    schedule_page[95] = new Interval(CLOSED, 5);
 
-    while (curr != start_limit){ 
+    //sets up the rest of the intervals
+    var start_limit = getStart(schedule_page, schedule_length); //start at unit 50
+    var curr = start_limit + schedule_page[start_limit].getSize();
+
+    while (curr != start_limit){
         if(schedule_page[curr] == null){
-            //there is nothing here. Create a new page
             var size = getSize(schedule_page, schedule_length, curr, start_limit);
-            schedule_page[curr] = new Task(OPEN, size, 0);
+            schedule_page[curr] = new Interval(OPEN, size);
             curr = (curr+size) % schedule_length;
         }else{
-            //there is an object here, open or closed. Lets move along and update curr.
-            var time = schedule_page[curr].getTime();
+            var time = schedule_page[curr].getSize();
             curr = (curr+time) % schedule_length;
         }
     }
-        printInfo(schedule_page);
+    //initial print
+    printInfo(schedule_page);
+
+    //now lets add a bunch of tasks to the schedule
 });
