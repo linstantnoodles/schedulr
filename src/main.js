b28ff39f9589bd8e1947acb3a096ae2c4c4140a2
alan <linpd2@gmail.com>
2013-02-03 16:52:03 -0500

    added handler to add new tasks

--- src/main.js
+++ src/main.js
@@ -1,4 +1,85 @@
 $(window).load(function() {
+    $(function() {
+        var name = $( "#name" ),
+          email = $( "#email" ),
+          password = $( "#password" ),
+          allFields = $( [] ).add( name ).add( email ).add( password ),
+          tips = $( ".validateTips" );
+     
+        function updateTips( t ) {
+          tips
+            .text( t )
+            .addClass( "ui-state-highlight" );
+          setTimeout(function() {
+            tips.removeClass( "ui-state-highlight", 1500 );
+          }, 500 );
+        }
+     
+        function checkLength( o, n, min, max ) {
+          if ( o.val().length > max || o.val().length < min ) {
+            o.addClass( "ui-state-error" );
+            updateTips( "Length of " + n + " must be between " +
+              min + " and " + max + "." );
+            return false;
+          } else {
+            return true;
+          }
+        }
+     
+        function checkRegexp( o, regexp, n ) {
+          if ( !( regexp.test( o.val() ) ) ) {
+            o.addClass( "ui-state-error" );
+            updateTips( n );
+            return false;
+          } else {
+            return true;
+          }
+        }
+     
+        $( "#dialog-form" ).dialog({
+          autoOpen: false,
+          height: 300,
+          width: 350,
+          modal: true,
+          buttons: {
+            "Create an account": function() {
+              var bValid = true;
+              allFields.removeClass( "ui-state-error" );
+     
+              /*bValid = bValid && checkLength( name, "username", 3, 16 );
+              bValid = bValid && checkLength( email, "email", 6, 80 );
+              bValid = bValid && checkLength( password, "password", 5, 16 );
+     
+              bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+              // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+              bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+              bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+                */ 
+              if ( bValid ) {
+                /*$( "#users tbody" ).append( "<tr>" +
+                  "<td>" + name.val() + "</td>" +
+                  "<td>" + email.val() + "</td>" +
+                  "<td>" + password.val() + "</td>" +
+                "</tr>" );*/
+                updateSchedule();
+                $( this ).dialog( "close" );
+              }
+            },
+            Cancel: function() {
+              $( this ).dialog( "close" );
+            }
+          },
+          close: function() {
+            allFields.val( "" ).removeClass( "ui-state-error" );
+          }
+        });
+     
+        $( "#create-user" )
+          .button()
+          .click(function() {
+            $( "#dialog-form" ).dialog( "open" );
+          });
+    });
 
     var OPEN = 1;
     var CLOSED = 0;
@@ -300,6 +381,14 @@ $(window).load(function() {
         }
     }
 
+    function updateSchedule(){
+         queue.push(new Task(OPEN, 200, 3));
+         processQueue(queue, waiting);
+         renderTasks(scheduler, schedule.tasks);
+         renderIntervals(scheduler, schedule.intervals);
+         scheduler.updateView();
+    }
+
 
     //set the closed limits
     schedule.intervals[50] = new Interval(CLOSED, 5);
@@ -328,12 +417,12 @@ $(window).load(function() {
     printInfo(schedule.tasks);
     printInfo(schedule.intervals);*/
 
-    queue.push(new Task(OPEN, 60, 2));
+    /*queue.push(new Task(OPEN, 60, 2));
     queue.push(new Task(OPEN, 300, 1));
     queue.push(new Task(OPEN, 200, 3));
     queue.push(new Task(OPEN, 100, 1));
     queue.push(new Task(OPEN, 30, 5));
-    queue.push(new Task(OPEN, 40, 2));
+    queue.push(new Task(OPEN, 40, 2));*/
     /*queue.push(new Task(OPEN, 10, 4));
     queue.push(new Task(OPEN, 10, 0)); //testing split
     queue.push(new Task(OPEN, 2, 5));
@@ -346,13 +435,14 @@ $(window).load(function() {
     /*queue.push(new Task(OPEN, 100, 5));
     queue.push(new Task(OPEN, 75, 5));
     queue.push(new Task(OPEN, 75, 5));*/
+
     processQueue(queue, waiting);
     printInfo(schedule.intervals);
     console.log("print waiting");
     printInfo(waiting);
     printFragmentationInfo();
 
-     /*   cal.addmarkedtimespan({
+    /*   cal.addmarkedtimespan({
                     days:  new date(),
                     zones: [zone_start, zone_end],
                     css:   "blue_section",
